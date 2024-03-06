f9d845fa5ad8a312e7e3d7a5b121605f52980420
Manoj Prabhakar <manojptn16@gmail.com>
2024-03-06 16:00:16 +0530

    ok2

--- lib/desktop_view.dart
+++ lib/desktop_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'dashboard.dart';
+import 'mobile_view.dart';
 
 class DesktopScaffold extends StatefulWidget {
   const DesktopScaffold({Key? key}) : super(key: key);
@@ -17,69 +18,203 @@ class _DesktopScaffoldState extends State<DesktopScaffold> {
   Color dashboardButtonColor = Colors.blue;
 
   // List of drawer items
+  bool isDrawerOpen = true; // Open the drawer by default
+  int _currentPageIndex = 0;
+  late PageController _pageController;
+
   final List<DrawerItem> drawerItems = [
-    DrawerItem(icon: Icons.shopping_cart, title: 'Orders', onTap: () {
-      // Navigate to orders screen
-    }),
-    DrawerItem(icon: Icons.attach_money, title: 'Ready to Dispatch', onTap: () {
-      // Navigate to revenue & expense screen
-    }),
-    DrawerItem(icon: Icons.money, title: 'Billing & Changes', onTap: () {
-      // Navigate to billing & changes screen
-    }),
-    DrawerItem(icon: Icons.settings, title: 'Settings', onTap: () {
-      // Navigate to settings screen
-    }),
+    DrawerItem(
+      icon: Icons.shopping_cart,
+      title: 'Orders',
+      page: OrdersPage(),
+      notificationCount: 5, // Example: set the notification count to 5
+    ),
+    DrawerItem(
+      icon: Icons.attach_money,
+      title: 'Ready to Dispatch',
+      page: RevenueExpensePage(),
+    ),
+    DrawerItem(
+      icon: Icons.money,
+      title: 'Billing & Changes',
+      page: BillingChangesPage(),
+    ),
+    DrawerItem(
+      icon: Icons.settings,
+      title: 'Settings',
+      page: SettingsPage(),
+    ),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _currentPageIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        elevation: 0,
+        title: Text(
+          'iLaundry',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        leading: Image.asset('assets/i.png'), // Replace 'your_image.png' with your image path
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              // Implement search functionality
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {
+              // Implement notification functionality
+            },
+          ),
+        ],
+      ),
       body: Row(
         children: <Widget>[
-          Drawer(
-            child: ListView(
-              padding: EdgeInsets.zero,
+          // Drawer
+          Container(
+            width: MediaQuery.of(context).size.width * 0.2, // 20% width of screen
+            color: Colors.white,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                  ),
-                  child: Text(
-                    'Dashboard',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                    ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.dashboard,
+                        color: Colors.green,
+                        size: 24,
+                      ),
+                      SizedBox(width: 8.0),
+                      Text(
+                        'Dashboard',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                // Create drawer items dynamically
-                for (var item in drawerItems)
+                Divider(
+                  color: Colors.grey,
+                ),
+                for (var i = 0; i < drawerItems.length; i++)
                   ListTile(
-                    leading: Icon(item.icon, size: 30),
-                    title: Text(item.title, style: TextStyle(fontSize: 18)),
-
+                    leading: Icon(
+                      drawerItems[i].icon,
+                      color: _currentPageIndex == i ? Colors.green : Colors.black,
+                    ),
+                    title: i == 0 ? _buildOrdersTileTitle(drawerItems[i]) : Text(drawerItems[i].title),
+                    onTap: () {
+                      setState(() {
+                        _currentPageIndex = i;
+                        _pageController.animateToPage(
+                          i,
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.ease,
+                        );
+                      });
+                    },
                   ),
               ],
             ),
           ),
+          // Page content
           Expanded(
-            child: Container(
-              // Your main content here
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: (index) {
+                setState(() {
+                  _currentPageIndex = index;
+                });
+              },
+              children: drawerItems.map((item) => item.page).toList(),
             ),
           ),
         ],
       ),
     );
   }
+
+  Widget _buildOrdersTileTitle(DrawerItem item) {
+    return Row(
+      children: [
+        Text(item.title),
+        SizedBox(width: 16), // Add some space between the text and the notification count
+        Container(
+          padding: EdgeInsets.all(4),
+          decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Text(
+            item.notificationCount.toString(),
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
 
-// Data model for drawer items
 class DrawerItem {
   final IconData icon;
   final String title;
-  final Function onTap;
+  final Widget page;
+  final int? notificationCount;
 
-  DrawerItem({required this.icon, required this.title, required this.onTap});
+  DrawerItem({required this.icon, required this.title, required this.page, this.notificationCount});
+}
+
+class RevenueExpensePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Display revenue and expense data
+    return Center(
+      child: Text('Revenue & Expense Page'),
+    );
+  }
+}
+
+class BillingChangesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Display billing and changes data
+    return Center(
+      child: Text('Billing & Changes Page'),
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Display settings data
+    return Center(
+      child: Text('Settings Page'),
+    );
+  }
 }
--- lib/tablet_view.dart
+++ lib/tablet_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 class TabletScaffold extends StatefulWidget {
   const TabletScaffold({Key? key}) : super(key: key);
 
@@ -10,71 +8,188 @@ class TabletScaffold extends StatefulWidget {
 }
 
 class _TabletScaffoldState extends State<TabletScaffold> {
-  var tilePadding = const EdgeInsets.only(left: 8.0, right: 8, top: 8);
-  bool showDashboard = false;
+  int _currentPageIndex = 0;
+  late PageController _pageController;
 
   final List<DrawerItem> drawerItems = [
-    DrawerItem(icon: Icons.shopping_cart, title: 'Orders', onTap: () {
-      // Navigate to orders screen
-    }),
-    DrawerItem(icon: Icons.attach_money, title: 'Ready to Dispatch', onTap: () {
-      // Navigate to revenue & expense screen
-    }),
-    DrawerItem(icon: Icons.money, title: 'Billing & Changes', onTap: () {
-      // Navigate to billing & changes screen
-    }),
-    DrawerItem(icon: Icons.settings, title: 'Settings', onTap: () {
-      // Navigate to settings screen
-    }),
+    DrawerItem(
+      icon: Icons.shopping_cart,
+      title: 'Orders',
+      page: OrdersPage(),
+      notificationCount: 5, // Example: set the notification count to 5
+    ),
+    DrawerItem(
+      icon: Icons.attach_money,
+      title: 'Ready to Dispatch',
+      page: RevenueExpensePage(),
+    ),
+    DrawerItem(
+      icon: Icons.money,
+      title: 'Billing & Changes',
+      page: BillingChangesPage(),
+    ),
+    DrawerItem(
+      icon: Icons.settings,
+      title: 'Settings',
+      page: SettingsPage(),
+    ),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _currentPageIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        elevation: 0,
+        title: Text(
+          'iLaundry',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        leading: Image.asset('assets/i.png'), // Replace 'your_image.png' with your image path
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              // Implement search functionality
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {
+              // Implement notification functionality
+            },
+          ),
+        ],
+      ),
       body: Row(
         children: <Widget>[
+          // Drawer
           Drawer(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: <Widget>[
-                DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
+            child: ListView.builder(
+              itemCount: drawerItems.length,
+              itemBuilder: (context, index) {
+                final item = drawerItems[index];
+                return ListTile(
+                  leading: Icon(
+                    item.icon,
+                    color: _currentPageIndex == index ? Colors.green : Colors.black,
                   ),
-                  child: Text(
-                    'Dashboard',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                    ),
-                  ),
-                ),
-                // Create drawer items dynamically
-                for (var item in drawerItems)
-                  ListTile(
-                    leading: Icon(item.icon, size: 30),
-                    title: Text(item.title, style: TextStyle(fontSize: 18)),
-
-                  ),
-              ],
+                  title: index == 0 ? _buildOrdersTileTitle(item) : Text(item.title),
+                  onTap: () {
+                    setState(() {
+                      _currentPageIndex = index;
+                      _pageController.animateToPage(
+                        index,
+                        duration: Duration(milliseconds: 300),
+                        curve: Curves.ease,
+                      );
+                    });
+                    Navigator.pop(context); // Close the drawer
+                  },
+                );
+              },
             ),
           ),
+          // Page content
           Expanded(
-            child: Container(
-              // Your main content here
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: (index) {
+                setState(() {
+                  _currentPageIndex = index;
+                });
+              },
+              children: drawerItems.map((item) => item.page).toList(),
             ),
           ),
         ],
       ),
     );
+  }
 
+  Widget _buildOrdersTileTitle(DrawerItem item) {
+    return Row(
+      children: [
+        Text(item.title),
+        SizedBox(width: 16), // Add some space between the text and the notification count
+        if (item.notificationCount != null)
+          Container(
+            padding: EdgeInsets.all(4),
+            decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Text(
+              item.notificationCount!.toString(),
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+      ],
+    );
   }
 }
+
 class DrawerItem {
   final IconData icon;
   final String title;
-  final Function onTap;
+  final Widget page;
+  final int? notificationCount;
 
-  DrawerItem({required this.icon, required this.title, required this.onTap});
+  DrawerItem({required this.icon, required this.title, required this.page, this.notificationCount});
+}
+
+class RevenueExpensePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Display revenue and expense data
+    return Center(
+      child: Text('Revenue & Expense Page'),
+    );
+  }
+}
+
+class BillingChangesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Display billing and changes data
+    return Center(
+      child: Text('Billing & Changes Page'),
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Display settings data
+    return Center(
+      child: Text('Settings Page'),
+    );
+  }
+}
+
+class OrdersPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Display orders data
+    return Center(
+      child: Text('Orders Page'),
+    );
+  }
 }
